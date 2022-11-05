57367f9feb65cc2c0e10ec4394a2045b68847b79
xqft <estefano.bargas@fing.edu.uy>
2022-11-05 18:00:15 -0300

    Last fix namespace

--- src/graphics/camera.cpp
+++ src/graphics/camera.cpp
@@ -2,7 +2,10 @@
 
 #include "camera.hpp"
 
-glm::mat4 Camera::lookat()
+namespace graphics
 {
-    return glm::lookAt(this->position, this->target, this->up);
+    glm::mat4 Camera::lookat()
+    {
+        return glm::lookAt(this->position, this->target, this->up);
+    }
 }
