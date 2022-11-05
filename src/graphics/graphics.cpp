c0b031dc839b034d84ee9e72a183a23d98b63d97
xqft <estefano.bargas@fing.edu.uy>
2022-11-05 17:57:45 -0300

    Fixed some graphics.cpp unused warnings

--- src/graphics/graphics.cpp
+++ src/graphics/graphics.cpp
@@ -11,13 +11,15 @@ namespace grphs {
     }
 
     static void GLAPIENTRY gl_callback(GLenum source, GLenum type, GLuint id,
-                    GLenum severity, GLsizei length, const GLchar* message,
-                    const void* userParam)
+                    GLenum severity, __attribute__((unused)) GLsizei length, const GLchar* message,
+                    __attribute__((unused)) const void* userParam)
     {
         std::cerr << "GL CALLBACK : " <<
             "type = " << std::hex << type << 
             ", severity =  " << severity << 
             ", message = " << message << 
+            ", source = " << source <<
+            ", id = " << id <<
             (type == GL_DEBUG_TYPE_ERROR ? "** GL ERROR **" : "") << std::endl;
     }
 
