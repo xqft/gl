04f067063d5af141902cbb77236ec8ab6cca2a94
xqft <estefano.bargas@fing.edu.uy>
2022-11-05 17:55:00 -0300

    Testing

--- src/main.cpp
+++ src/main.cpp
@@ -4,5 +4,27 @@
 
 int main() {
     std::cout << "Hello world" << std::endl;
-    return 1;
+
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+    GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+    if (window == NULL)
+    {
+        std::cout << "Failed to create GLFW window" << std::endl;
+        glfwTerminate();
+        return -1;
+    }
+    glfwMakeContextCurrent(window);
+
+    if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+    {
+        std::cout << "Failed to initialize GLAD" << std::endl;
+        return -1;
+    } 
+
+    while (true) {}
+    return 0;
 }
