4650d0f0650cdb1ccfa197275c6babd12fa67b34
xqft <estefano.bargas@fing.edu.uy>
2022-11-05 17:55:00 -0300

    Final setup for the day

--- src/main.cpp
+++ src/main.cpp
@@ -6,8 +6,8 @@ int main() {
     std::cout << "Hello world" << std::endl;
 
     glfwInit();
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 6);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
 
     GLFWwindow* window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
